619cce3763044336ede270b8afd980dc5c98f4b3
Ahmad Alfawwaz <ahmadulfawwaz@gmail.com>
2021-12-16 19:45:30 +0100

    Define solidity abi json types

---
+++ lib/types/abi-types.ts
@@ -0,0 +1,44 @@
+export type SolidityABI = SolidityDefinition[]
+type SolidityStateMutability = 'pure' | 'view' | 'payable' | 'nonpayable'
+type SolidityDefinition = FunctionDescription | EventDescription | ErrorDescription
+
+export interface CompilationOutput {
+    _format: string,
+    contractName: string,
+    sourceName: string,
+    abi: SolidityABI,
+    bytecode: string,
+    deployedBytecode: string,
+    linkReferences: any,
+    deployedLinkReferences: any
+}
+
+export interface FunctionDescription {
+    name?: string
+    type: 'function' | 'constructor' | 'receive' | 'fallback'
+    inputs?: SolidityType[]
+    outputs?: SolidityType[]
+    stateMutability?: SolidityStateMutability
+}
+
+export interface EventDescription {
+    name: string
+    type: 'event'
+    inputs?: SolidityType[]
+    anonymous?: boolean,
+}
+
+export interface ErrorDescription {
+    name: string
+    type: 'error'
+    inputs?: SolidityType[]
+    anonymous?: boolean,
+}
+
+export interface SolidityType {
+    components?: SolidityType[],
+    indexed?: boolean,
+    internalType?: string,
+    name: string,
+    type: string
+}
